0caa68d79ba9a4ad10b5c7140eb1d06ad0c14ffe
Karthick M <mkarthickakmn@gmail.com>
2022-09-11 17:39:40 +0530

    intersection observer and images

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -7,10 +7,58 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
   constructor() { }
-  @Input() cardType:any;
-  @Input()color:any;
+  @Input() cardType: any;
+  @Input() color: any;
+
+  type1: any = [];
+  type2: any = [];
+  type4: any = [];
+
   ngOnInit(): void {
-    
+    this.type2 = [{
+      srcset: 'assets/img/Image147.png, assets/img/Image147@2x.png 2x', image: 'assets/img/Image147.png',
+      header: 'M3M Woodshire', desc: 'Pitt Street Kirribilli NSW 2061'
+    }, {
+      srcset: 'assets/img/Image148.png, assets/img/Image148@2x.png 2x', image: 'assets/img/Image148.png',
+      header: 'M3M Woodshire', desc: 'Pitt Street Kirribilli NSW 2061'
+    }, {
+      srcset: 'assets/img/Image146.png, assets/img/Image146@2x.png 2x', image: 'assets/img/Image146.png',
+      header: 'M3M Woodshire', desc: 'Pitt Street Kirribilli NSW 2061'
+    }, {
+      srcset: 'assets/img/Image145.png, assets/img/Image145@2x.png 2x', image: 'assets/img/Image145.png',
+      header: 'M3M Woodshire', desc: 'Pitt Street Kirribilli NSW 2061'
+    }, {
+      srcset: 'assets/img/Image144.png, assets/img/Image144@2x.png 2x', image: 'assets/img/Image144.png',
+      header: 'M3M Woodshire', desc: 'Pitt Street Kirribilli NSW 2061'
+    }];
+
+    this.type1 = [{
+      srcset: 'assets/img/Image139.png, assets/img/Image139@2x.png 2x', image: 'assets/img/Image139.png',
+      header: 'Boston House, Andheri East'
+    }, {
+      srcset: 'assets/img/Image140.png, assets/img/Image140@2x.png 2x', image: 'assets/img/Image140.png',
+      header: 'Boston House, Andheri East'
+    }, {
+      srcset: 'assets/img/Image141.png, assets/img/Image141@2x.png 2x', image: 'assets/img/Image141.png',
+      header: 'Boston House, Andheri East'
+    }, {
+      srcset: 'assets/img/Image143.png, assets/img/Image143@2x.png 2x', image: 'assets/img/Image143.png',
+      header: 'Boston House, Andheri East'
+    }];
+
+    this.type4 = [{
+      srcset: 'assets/img/Image156.png, assets/img/Image156@2x.png 2x', image: 'assets/img/Image156.png',
+      header: 'Awards, Happenings, Spaces', desc: 'Work from any corner on the entire floor without any interruption. Work from any corner on the entire floor without any interruption'
+    }, {
+      srcset: 'assets/img/Image157.png, assets/img/Image157@2x.png 2x', image: 'assets/img/Image157.png',
+      header: 'Awards, Happenings, Spaces', desc: 'Work from any corner on the entire floor without any interruption. Work from any corner on the entire floor without any interruption'
+    }, {
+      srcset: 'assets/img/Image158.png, assets/img/Image158@2x.png 2x', image: 'assets/img/Image158.png',
+      header: 'Awards, Happenings, Spaces', desc: 'Work from any corner on the entire floor without any interruption. Work from any corner on the entire floor without any interruption'
+    }, {
+      srcset: 'assets/img/Image159.png, assets/img/Image159@2x.png 2x', image: 'assets/img/Image159.png',
+      header: 'Awards, Happenings, Spaces', desc: 'Work from any corner on the entire floor without any interruption. Work from any corner on the entire floor without any interruption'
+    }];
   }
 
 }
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -5,16 +5,48 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements OnInit ,AfterViewInit{
+export class LandingPageComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+
     
   }
 
   ngAfterViewInit(): void {
+ 
+    {
+      const sectionHeroEl = document.querySelector("header") as HTMLElement;
+      let header = document.getElementsByClassName("header");
+      let headersection = document.getElementsByClassName("header-section");
   
+      const obs = new IntersectionObserver(
+        function (entries) {
+          const ent = entries[0];
+          console.log(ent);
+  
+          if (ent.isIntersecting === false) {
+            header[0].classList.add('sticky1');
+            headersection[0].classList.add('sticky2');
+  
+          }
+  
+          if (ent.isIntersecting === true) {
+            header[0].classList.remove('sticky1');
+            headersection[0].classList.remove('sticky2');
+  
+          }
+        },
+        {
+          // In the viewport
+          root: null,
+          threshold: 0,
+          rootMargin:'-90.4px'
+        }
+      );
+      obs.observe(sectionHeroEl);
+    }
   }
 
 }
