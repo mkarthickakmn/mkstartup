ca9501941c223058621c8c41b15f876aeea334f2
Karthick M <mkarthickakmn@gmail.com>
2022-09-13 01:57:48 +0530

    add images card

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent implements OnInit {
+export class CardsComponent implements OnInit,AfterViewInit {
   constructor() { }
   @Input() cardType: any;
   @Input() color: any;
@@ -46,6 +46,21 @@ export class CardsComponent implements OnInit {
       header: 'Boston House, Andheri East'
     }];
 
+    
+    // this.type1 = [{
+    //   srcset: 'assets/img/Image139@2x.png', image: 'assets/img/Image139.png',
+    //   header: 'Boston House, Andheri East'
+    // }, {
+    //   srcset: 'assets/img/Image140@2x.png', image: 'assets/img/Image140.png',
+    //   header: 'Boston House, Andheri East'
+    // }, {
+    //   srcset: 'assets/img/Image141@2x.png', image: 'assets/img/Image141.png',
+    //   header: 'Boston House, Andheri East'
+    // }, {
+    //   srcset: 'assets/img/Image143@2x.png', image: 'assets/img/Image143.png',
+    //   header: 'Boston House, Andheri East'
+    // }];
+
     this.type4 = [{
       srcset: 'assets/img/Image156.png, assets/img/Image156@2x.png 2x', image: 'assets/img/Image156.png',
       header: 'Awards, Happenings, Spaces', desc: 'Work from any corner on the entire floor without any interruption. Work from any corner on the entire floor without any interruption'
@@ -59,6 +74,17 @@ export class CardsComponent implements OnInit {
       srcset: 'assets/img/Image159.png, assets/img/Image159@2x.png 2x', image: 'assets/img/Image159.png',
       header: 'Awards, Happenings, Spaces', desc: 'Work from any corner on the entire floor without any interruption. Work from any corner on the entire floor without any interruption'
     }];
+
+     
+  }
+
+  ngAfterViewInit(): void {
+
+    {
+     
+
+    
+    }
   }
 
 }
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -11,42 +11,68 @@ export class LandingPageComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void {
 
-    
+
   }
 
   ngAfterViewInit(): void {
- 
+
     {
-      const sectionHeroEl = document.querySelector("header") as HTMLElement;
+      const sectionHeroEl = document.querySelector("header");
       let header = document.getElementsByClassName("header");
       let headersection = document.getElementsByClassName("header-section");
-      let height=header[0].getBoundingClientRect().height;
+      let height = header[0].getBoundingClientRect().height;
       console.log(height);
       const obs = new IntersectionObserver(
         function (entries) {
           const ent = entries[0];
           console.log(ent);
-  
+
           if (ent.isIntersecting === false) {
             header[0].classList.add('sticky1');
             headersection[0].classList.add('sticky2');
-  
+
           }
-  
+
           if (ent.isIntersecting === true) {
             header[0].classList.remove('sticky1');
             headersection[0].classList.remove('sticky2');
-  
+
           }
         },
         {
           // In the viewport
           root: null,
           threshold: 0,
-          rootMargin:`-90.4px`
+          rootMargin: `-90.4px`
         }
       );
-      obs.observe(sectionHeroEl);
+      obs.observe(sectionHeroEl as HTMLElement);
+
+      //////////////////////////
+
+    //   const imgs = document.querySelectorAll('img[data-src]');
+    //   const imgLazy = new IntersectionObserver(function (entries: any, observer) {
+    //     const [entry] = entries;
+    //     console.log(entries);
+    //     if (!entry.isIntersecting) return;
+
+    //     // Replace src with data-src
+    //     entry.target.src = entry.target.dataset.src;
+    //     console.log(entry.target.src);
+    //     entry.target.addEventListener('load', function () {
+    //       entry.target.classList.remove('lazy-img');
+    //     });
+
+    //     observer.unobserve(entry.target);
+
+    //   }, {
+    //     // In the viewport
+    //     root: null,
+    //     threshold: 0,
+    //     rootMargin: '-200px',
+    //   })
+
+    //   imgs.forEach(img => imgLazy.observe(img))
     }
   }
 
